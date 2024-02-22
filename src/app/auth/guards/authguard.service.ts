1a7a1eebc5b1c0f3893389516b46119795ceb91a
Silas Gomes da Silva <88496746+SilasGSilva@users.noreply.github.com>
2024-02-21 22:23:06 -0300

    Add files via upload

--- src/app/auth/guards/authguard.service.ts
+++ src/app/auth/guards/authguard.service.ts
@@ -1,1 +1,23 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
+import { Observable } from 'rxjs';
 
+@Injectable({
+  providedIn: 'root'
+})
+
+export class AuthGuardService implements CanActivate {
+
+  constructor(private _router: Router) { }
+
+  canActivate( route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean | Observable<boolean> | Promise<boolean> {
+
+    if (sessionStorage.getItem('token')) {
+      this._router.navigate(['eventsMonitor']);
+      return false;
+    }
+
+    return true;
+  }
+
+}
